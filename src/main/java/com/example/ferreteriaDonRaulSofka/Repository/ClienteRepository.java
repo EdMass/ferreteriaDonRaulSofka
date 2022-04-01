eddf55a5912a76b14a0fbd4e7443cbf2a790b43f
EdMass <zairmass@gmail.com>
2022-03-31 22:38:03 -0500

    se agrega metodo flux

--- src/main/java/com/example/ferreteriaDonRaulSofka/Repository/ClienteRepository.java
+++ src/main/java/com/example/ferreteriaDonRaulSofka/Repository/ClienteRepository.java
@@ -2,7 +2,8 @@ package com.example.ferreteriaDonRaulSofka.Repository;
 
 import com.example.ferreteriaDonRaulSofka.Model.Cliente;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface ClienteRepository extends ReactiveMongoRepository<Cliente, String> {
-
+    Flux<Cliente> findByIdCliente(String idCliente);
 }
